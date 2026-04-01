f1aed29e718fdf58eda1e4b21fa89794582f57c1
Aftab Aqueel Khan <aftabaqueelkhan@gmail.com>
2026-04-01 20:15:52 +0530

    [FIX] seting up detailed logs for errror

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, request, render_template
 import os
 import numpy as np
 import pandas as pd
+from src.logger import get_logger
 
 from sklearn.preprocessing import StandardScaler
 
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+log = get_logger(__name__)
 
 application = Flask(__name__)
 app = application
@@ -19,22 +22,28 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        data = CustomData(
-            gender = request.form.get('gender'),
-            race_ethnicity = request.form.get('ethnicity'),
-            parental_level_of_education = request.form.get('parental_level_of_education'),
-            lunch = request.form.get('lunch'),
-            test_preparation_course = request.form.get('test_preparation_course'),
-            reading_score = float(request.form.get('reading_score')),
-            writing_score = float(request.form.get('writing_score'))
-        )
-        pred_df = data.get_data_as_data_frame()
-        print(pred_df)
+        try:
+            data = CustomData(
+                gender = request.form.get('gender'),
+                race_ethnicity = request.form.get('ethnicity'),
+                parental_level_of_education = request.form.get('parental_level_of_education'),
+                lunch = request.form.get('lunch'),
+                test_preparation_course = request.form.get('test_preparation_course'),
+                reading_score = float(request.form.get('reading_score')),
+                writing_score = float(request.form.get('writing_score'))
+            )
+            pred_df = data.get_data_as_data_frame()
+            print(pred_df)
 
-        predict_pipeline = PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
-        predicted_score = round(float(results[0]), 2)
-        return render_template('home.html', results=predicted_score)
+            predict_pipeline = PredictPipeline()
+            results = predict_pipeline.predict(pred_df)
+            predicted_score = round(float(results[0]), 2)
+            return render_template('home.html', results=predicted_score)
+        except Exception as e:
+            import traceback
+            log.error(f"Error occurred: {e}", exc_info=True)
+            print(traceback.format_exc())
+            return f"Error: {e}"
 
 
 if __name__ == '__main__':
